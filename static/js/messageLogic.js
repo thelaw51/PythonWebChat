fc468f1cff8835618ff34990590fd9dec1f27db7
Joe <joegillard3@gmail.com>
2024-07-13 18:26:54 +0100

    moved messege logic from html file to seperate file

---
+++ static/js/messageLogic.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  var socket = io();
+
+  socket.on('connect', function() {
+      var username = prompt('Enter your username:');
+      var room = 'general';
+      socket.emit('join', {'username': username, 'room': room});
+  });
+
+  socket.on('message', function(msg) {
+      var messageContainer = document.getElementById('messages');
+      var messageElement = document.createElement('li');
+      messageElement.textContent = msg;
+      messageContainer.appendChild(messageElement);
+  });
+
+  function sendMessage() {
+      var messageInput = document.getElementById('messageInput');
+      var message = messageInput.value;
+      socket.send(message);
+      messageInput.value = '';
+  }
+
+  document.getElementById('sendButton').onclick = sendMessage;
+});
