9b0dc105259968198f0047288131436b6d499afb
Joe <joegillard3@gmail.com>
2024-07-13 16:29:08 +0100

    fixed dup code issue

--- app.py
+++ app.py
@@ -33,36 +33,3 @@ def on_leave(data):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
-
-print("Starting Flask app...")  # Add this line to check if script is being executed
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
-socketio = SocketIO(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@socketio.on('message')
-def handle_message(msg):
-    send(msg, broadcast=True)
-
-@socketio.on('join')
-def on_join(data):
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    emit('message', username + ' has entered the room.', room=room)
-
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    emit('message', username + ' has left the room.', room=room)
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
